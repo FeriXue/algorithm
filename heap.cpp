5cb00686be08bb83e3bf589e65a55d14542d6dfa
feri <2567219039@qq.com>
2020-12-11 22:32:34 +0800

    heap

--- heap.cpp
+++ heap.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 const int N = 1e5 + 10;
 
 int heapp[N], heap_size = 0;
+int ph[N], hp[N];
+
+void heap_swap(int a, int b)
+{
+    swap(ph[hp[a]], ph[hp[b]]);
+    swap(hp[a], hp[b]);
+    swap(heapp[a], heapp[b]);
+}
 
 void down(int x)
 {
@@ -16,36 +25,56 @@ void down(int x)
         smallest = 2 * x + 1;
     }
     if (smallest != x) {
-        swap(heapp[smallest], heapp[x]);
+        heap_swap(smallest, x);
         down(smallest);
     }
 }
 
 void up(int x)
 {
-    while (x / 2 && heapp[x / 2] < heapp[x]) {
-        swap(heapp[x], heapp[x / 2]);
+    while (x / 2 && heapp[x / 2] > heapp[x]) {
+        heap_swap(x, x / 2);
         x /= 2;
     }
 }
 
 int main()
 {
-    int n = 0, m = 0;
-    cin >> n >> m;
-    heap_size = n;
-    for (int i = 1; i <= n; ++ i) {
-        scanf("%d", &heapp[i]);
-    }
-
-    for (int i = n / 2; i; -- i) {
-        down(i);
-    }
-
-    while (m --) {
-        printf("%d ", heapp[1]);
-        heapp[1] = heapp[heap_size--];
-        down(1);
+    int n = 0, now_in = 0;
+    string op;
+    cin >> n;
+    while (n --) {
+        cin >> op;
+        if (op == "I") {
+            int x = 0;
+            cin >> x;
+            ++ heap_size;
+            ++ now_in;
+            ph[now_in] = heap_size;
+            hp[heap_size] = now_in;
+            heapp[heap_size] = x;
+            up(heap_size);
+        } else if (op == "PM") {
+            printf("%d\n", heapp[1]);
+        } else if (op == "DM") {
+            heap_swap(1, heap_size);
+            -- heap_size;
+            down(1);
+        } else if (op == "D") {
+            int k = 0;
+            cin >> k;
+            int heap_pos = ph[k]; // 得到第k个插入的数在堆中的位置;
+            heap_swap(ph[k], heap_size);
+            -- heap_size;
+            down(heap_pos); // 如果这里的参数写成了ph[k]那么其实down的是最后一个已经不存在的堆中的位置因为在之前元素对调的时候相应的ph数组中第k个插入元素的位置已经被改变了
+            up(heap_pos);
+        } else if (op == "C") {
+            int x = 0, k = 0;
+            cin >> k >> x;
+            heapp[ph[k]] = x;
+            down (ph[k]);
+            up(ph[k]);
+        }
     }
     return 0;
 }
