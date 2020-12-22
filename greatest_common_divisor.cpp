8b7a0fc23cec411b8b1d8080c6d1ff0b5114216b
feri <2567219039@qq.com>
2020-12-22 18:50:59 +0800

    欧几里得算法（扩展相除法）, 注意数论的基础理论

---
+++ greatest_common_divisor.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+    return b != 0 ? gcd(b, a % b) : a;
+}
+
+int main()
+{
+    int n = 0;
+    cin >> n;
+    int a = 0, b = 0;
+
+    while (n --) {
+        cin >> a >> b;
+        cout << gcd(a, b) << endl;
+    }
+
+    return 0;
+}
+
