4c7166a6db71b373e9f6398616745629f8200a71
feri <2567219039@qq.com>
2020-12-22 23:13:56 +0800

    质数的各种筛法

--- select_prime.cpp
+++ select_prime.cpp
@@ -34,8 +34,12 @@ void se_prime(int n)
             prime[cnt ++] = i;
         }
         // 两种情况，i是合数 && i是质数
-        for (int j = 0; j < cnt && prime[j] <= n / i; ++ j) { // 当质数大于n/i的时候就break掉   
+        for (int j = 0; prime[j] <= n / i; ++ j) { // 当质数大于n/i的时候就break掉   
+            // 每一次把当前的质数和i的乘积筛掉
+            // 只用最小质因子来筛掉某一个数;
+            // 对于一个合数x，假设pj是x的最小质因子，当i枚举到x/pj的时候就一定会把x筛掉
             st[prime[j] * i] = 1; // 合数pj * i只会被它的最小质因子筛掉；            
+            // 当这句话成立时，意味着pj一定是i最小质因子；
             if (i % prime[j] == 0) { // 意味着prime数组已经走到了最后一个元素        
                 // 当pj是i的最小质因子的时，++ i；                                   
                 // i % pj == 0  pj一定是i的最小质因子，pj一定是pj * i的最小质因子；  
@@ -44,7 +48,19 @@ void se_prime(int n)
             }                                                                        
         }                                                                            
     }
-    //
+    // 线性筛法重写
+    
+    for (int i = 2; i <= n; ++ i) {
+        if (!st[i]) {
+            prime[cnt ++] = i;
+        }
+        for (int j = 0; prime[j] <= n / i; ++ j) {
+            st[prime[j] * i] = 1;
+            if (i % prime[j] == 0) {
+                break;
+            }
+        }
+    }
 }
 
 
