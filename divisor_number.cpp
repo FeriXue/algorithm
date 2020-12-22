055307058c7f809de31961e220834be4d911f9c1
feri <2567219039@qq.com>
2020-12-22 17:48:39 +0800

    求一个数的约数个数，注意公式使用即可

---
+++ divisor_number.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+const int mod = 1e9 + 7;
+
+int main()
+{
+    unordered_map<int, int> hashh;
+    int n = 0;
+    cin >> n;
+    int x = 0;
+    while (n --) {
+        cin >> x;
+        for (int i = 2; i <= x / i; ++ i) {
+            while (x % i == 0) {
+                x /= i;
+                ++ hashh[i];
+            }
+        }
+        if (x > 1) {
+            ++ hashh[x];
+        }
+    }
+
+    long long res = 1;
+    for (auto &c : hashh) {
+        res = res * (c.second + 1) % mod;
+    }
+
+    cout << res << endl;
+    return 0;
+}
+
