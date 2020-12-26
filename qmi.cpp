56cc14674429e5fe772446e126dadf3d5f86c0f6
feri <2567219039@qq.com>
2020-12-26 20:38:50 +0800

    快速幂

--- qmi.cpp
+++ qmi.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 
 using namespace std;
-using LL = long long; 
+using LL = long long;
 
-int qmi(int a, int k, int p)
+int get_qmi(int a, int k, int p)
 {
     LL res = 1;
     while (k) {
         if (k & 1) {
-            res = res * a % p; 
+            res = res * a % p;
         }
         k >>= 1;
-        a = (LL)a * a % p;
+        a = (LL) a * a % p;
     }
 
     return res;
@@ -19,6 +19,13 @@ int qmi(int a, int k, int p)
 
 int main()
 {
+    int n = 0;
+    scanf("%d", &n);
+    int a = 0, k = 0, p = 0;
+
+    while (n --) {
+        scanf("%d%d%d", &a, &k, &p);
+        cout << get_qmi(a, k, p) << endl;
+    } 
     return 0;
 }
-
