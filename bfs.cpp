e034836de5760b250e26da10d6b9e1a3b81cee2a
feri <2567219039@qq.com>
2020-12-28 19:20:29 +0800

    并且输出路径

--- bfs.cpp
+++ bfs.cpp
@@ -9,11 +9,13 @@ const int N = 110;
 int n, m;
 int maze[N][N], dis[N][N];
 PII que[N * N];
+PII pre[N][N];
 
 int bfs()
 {
     memset(dis, 0xff, sizeof(dis));
     dis[0][0] = 0;
+    pre[0][0] = {0, 0};
 
     int hh = 0, tt = 0;
     que[0] = {0, 0};
@@ -25,11 +27,19 @@ int bfs()
         for (int i = 0; i < 4; ++ i) {
             int x = t.first + dx[i], y = t.second + dy[i];
             if (x >= 0 && y >= 0 && x < n && y < m && dis[x][y] == -1 && maze[x][y] == 0) {
+                pre[x][y] = {t.first, t.second};
                 dis[x][y] = dis[t.first][t.second] + 1;
                 que[ ++ tt] = {x, y};
             }
         }
     }
+    
+    auto temp = pre[n - 1][ m - 1];
+    while (temp.first != 0 || temp.second != 0) {
+        cout << temp.first << " " << temp.second << endl;
+        temp = pre[temp.first][temp.second];
+    }
+
     return dis[n - 1][m - 1];
 }
 
