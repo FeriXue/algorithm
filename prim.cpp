09d865161b7589871261e2cce482a55b64275958
feri <2567219039@qq.com>
2021-01-03 15:46:37 +0800

    prim算法求最小生成树

--- prim.cpp
+++ prim.cpp
@@ -3,61 +3,61 @@
 
 using namespace std;
 
-const int N = 510, INF = 0x3f3f3f3f; 
+const int N = 510, INF = 0x3f3f3f3f;
 
 int g[N][N];
 int dist[N];
 bool st[N];
-
 int n, m;
 
 int prim()
 {
     memset(dist, 0x3f, sizeof(dist));
-    int res = 0;
+
+    int res = 0; // 返回最小生成树的权值和;
 
     for (int i = 0; i < n; ++ i) {
         int t = -1;
-        for (int j = 1; j <= n; ++ j) {
-            if (!(st[j]) && (t == -1 || dist[t] > dist[j])) {
+        for (int j = 1; j <= n; ++ j) { // 找到不在集合中距离集合最近的点t
+            if (!st[j] && (t == -1 || dist[t] > dist[j])) {
                 t = j;
             }
         }
-        
-        if (i && dist[t] == INF) { // 如果不是第一个点并且距离集合最近的点的距离为INF说明没有生成树;
+
+        if (i && dist[t] == INF) {
             return INF;
         }
+
         if (i) {
-            res += dist[t]; // 这一步要放在使用t更新其他点的前面，因为可能存在自环并且边权值为负数，可能会自己更新自己；
+            res += dist[t];
         }
 
-        st[t] = 1;
-
-        for (int j = 1; j <= n; ++ j) {
+        st[t] = 1; // 将点t加入集合中;
+        for (int j = 1; j <= n; ++ j) { // 使用点t更新其他的点
             dist[j] = min(dist[j], g[t][j]);
         }
     }
+
     return res;
 }
 
 int main()
 {
     memset(g, 0x3f, sizeof(g));
-
     cin >> n >> m;
     int a = 0, b = 0, c = 0;
-    for (int i = 0; i < n; ++ i) {
+
+    while (m --) {
         cin >> a >> b >> c;
         g[a][b] = g[b][a] = min(g[a][b], c);
     }
 
     int t = prim();
+
     if (t == INF) {
         cout << "impossible" << endl;
     } else {
         cout << t << endl;
     }
-
     return 0;
 }
-
