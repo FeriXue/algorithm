ed1f04146156bb867ab8838f69f407e5e10c161a
feri <2567219039@qq.com>
2021-01-21 11:33:45 +0800

    leetcode 53 线性dp contiguous subarray which has the largest sum

--- leetcode/53.cpp
+++ leetcode/53.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <cstring>
 
-int main()
-{
-    return 0;
-}
+using namespace std;
 
+class Solution {
+public:
+    const int N = 3e4 + 10;
+    int maxSubArray(vector<int>& a) {
+        int f[N];
+        memset(f, 0, sizeof (f));
+        f[0] = a[0];
+        for (int i = 1; i < (int)a.size(); ++ i) {
+            f[i] = a[i];
+            f[i] = max(f[i - 1] + a[i], f[i]);
+        }
+
+        int res = -1e5 - 1;
+        for (int i = 0; i < (int)a.size(); ++ i) {
+            res = max(res, f[i]);
+        }
+        return res;
+    }
+};
