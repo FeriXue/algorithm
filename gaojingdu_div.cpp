8bb093593af76683e70d47d0097ce1ccfddbb2bc
feri <2567219039@qq.com>
2021-01-25 21:40:46 +0800

    复习高精度除法有所注释

--- gaojingdu_div.cpp
+++ gaojingdu_div.cpp
@@ -9,9 +9,9 @@ vector<int> div(vector<int> A, int b, int &r)
     vector<int> C;
 
     for (int i = A.size() - 1; i >= 0; --i) {
-        r = r * 10 + A[i];
-        C.push_back(r / b);
-        r = r % b;
+        r = r * 10 + A[i]; // 借位乘10，从个位开始除
+        C.push_back(r / b); // 假如不够除就是商0 
+        r = r % b; // 余数
     }
     reverse(C.begin(), C.end());
 
@@ -23,6 +23,7 @@ vector<int> div(vector<int> A, int b, int &r)
 
 int main()
 {
+    // 给定两个整数a, b 计算a / b 的商和余数；
     string a;
     int b = 0;
     cin >> a >> b;
@@ -32,7 +33,7 @@ int main()
         A.push_back(a[i] - '0');
     }
 
-    int r = 0;
+    int r = 0; // 表示余数
     auto C = div(A, b, r);
 
     for (int i = C.size() - 1; i >= 0; --i) {
