c0f6b307607f6229b8724cc8d9eb031aaada497b
feri <2567219039@qq.com>
2021-02-15 21:09:55 +0800

    过完年啦，来个简单题上手

---
+++ leetcode/155.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stack>
+#include <limits.h>
+
+using namespace std;
+
+class MinStack {
+public:
+    /** initialize your data structure here. */
+
+    MinStack() {
+        mi.push(INT_MAX);
+    }
+    
+    void push(int x) {
+        x_stack.push(x);
+        mi.push(min(x, mi.top()));
+    }
+    
+    void pop() {
+        x_stack.pop(); 
+        mi.pop();
+    }
+    
+    int top() {
+        return x_stack.top();
+    }
+    
+    int getMin() {
+        return mi.top();
+    }
+
+private:
+    stack<int> x_stack;
+    stack<int> mi;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
