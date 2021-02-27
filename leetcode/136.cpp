f1cb14f8f18397345f793ffcab0392c802778acc
feri <2567219039@qq.com>
2021-02-27 21:04:49 +0800

    异或运算的性质

--- leetcode/136.cpp
+++ leetcode/136.cpp
@@ -8,6 +8,9 @@ using namespace std;
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        // 任何数和0异或仍然等于本身
+        // 任何数和本身异或得到0
+        // 异或运算满足交换律和结合律a^b^a=b^a^a=b^(a^a)=b^0=b
         int ans = 0;
         for (auto &c : nums) {
             ans ^= c;
